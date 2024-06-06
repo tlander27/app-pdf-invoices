44369f1d7373b76452e0c94418d1e1ce3d2dbcd2
joshua <jrt@tellander.net>
2024-06-06 15:56:21 -0500

    Add header, total price, company name and image.

--- main.py
+++ main.py
@@ -39,4 +39,22 @@ for file in files:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    # Display total price
+    total_sum = df["total_price"].sum()
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=65, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.ln(h=8)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+    pdf.ln(h=8)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=14, txt=f"CompanyName")
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"PDFs/{pdf_name}.pdf")
